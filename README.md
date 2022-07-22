010988305ea203c596b7e5d93715f0d27e1b4326
Andrew <paintstone@gmail.com>
2022-07-21 19:57:12 -0700

    updated a few line spaces and roughly translated comments to english which related to changes in code spaces to tabs, moved to 3.8 anaconda

---
+++ README.md
@@ -0,0 +1,12 @@
+## Automated-Trading-System
+# Cross
+
+
+# Install
+1. into your terminal type 'conda env create -f environment.yml'
+
+
+# Known Bugs
+1. no requirements.txt, added 0822
+2. ENVIRONMENT.yml compatibility and file missing, added 0822
+3. TA-Lib and backtrader not working
--- Strategy_definition.py
+++ Strategy_definition.py
@@ -7,105 +7,125 @@ Created on Tue Nov 24 21:38:06 2020
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 import pandas as pd
 import os
+import numpy
 import backtrader as bt
 import backtrader.indicators as btind
 import backtrader.analyzers as btanalyzers
 import datetime
-import os.path 
-import sys 
+import os.path
+import sys
 import matplotlib
+import talib #as TA-Lib
 from pandas_datareader import data, wb
 import yfinance as yf
 
-# Create a Strategy Test
+
+# TODO wtf pandas_datareader doesn't exist but pandas-datareader does but isn't recognized
+# TODO why isn't the 'import backtrader' working, not thru idea, not thru requirement.txt
+# TODO pip install backtrader py35 as per https://www.backtrader.com/docu/installation/
+# TODO change to py36 or try 'pip install bta-lib'?
+
+
 class TestStrategy(bt.Strategy):
+	"""Create a Strategy Test"""
+	def log(self, txt, dt=None):
+		''' Logging function fot this strategy'''
+		dt = dt or self.datas[0].datetime.date(0)
+		print('%s, %s' % (dt.isoformat(), txt))
 
-    def log(self, txt, dt=None):
-        ''' Logging function fot this strategy'''
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+	def __init__(self):
+		"""Keep a reference to the "close" line in the data[0] dataseries"""
+		self.dataclose = self.datas[0].close
 
-    def __init__(self):
-        # Keep a reference to the "close" line in the data[0] dataseries
-        self.dataclose = self.datas[0].close
+	def next(self):
+		"""Simply log the closing price of the series from the reference"""
+		self.log('Close, %.2f' % self.dataclose[0])
 
-    def next(self):
-        # Simply log the closing price of the series from the reference
-        self.log('Close, %.2f' % self.dataclose[0])
-
-        if self.dataclose[0] < self.dataclose[-1]:
-            # current close less than previous close
+		if self.dataclose[0] < self.dataclose[-1]:
+			# current close less than previous close
 
             if self.dataclose[-1] < self.dataclose[-2]:
-                # previous close less than the previous close
+				# previous close less than the previous close
 
                 # BUY, BUY, BUY!!! (with all possible default parameters)
                 self.log('BUY CREATE, %.2f' % self.dataclose[0])
-                self.buy()
-    
+				self.buy()
 
 
-# Create a Double Crossover Strategy
+
 class Cross_Medie(bt.Strategy):
-    #definiamo la media veloce e la media lenta
+	"""
+	Create a Double Crossover Strategy
+	"""
+	"""We define the fast average and the slow average"""
+	#definiamo la media veloce e la media lenta
     params = (('Med_vel', 50), ('Med_len', 100))
 
-    #inizializziamo le due medie
+	# inizializziamo le due medie
     def __init__(self):
-        self.sma_vel = btind.SMA(period=self.p.Med_vel)
-        self.sma_len = btind.SMA(period=self.p.Med_len)
+		"""we initialize the two averages"""
+		self.sma_vel = btind.SMA(period=self.p.Med_vel)
+		self.sma_len = btind.SMA(period=self.p.Med_len)
 
-    #definiamo il segnale di acquisto/vendita
+		# definiamo il segnale di acquisto/vendita
+        # we define the buy / sell signal
         self.buysig = btind.CrossOver(self.sma_vel, self.sma_len)
-        
-        # salviamo di dati di closing
+
+		# salviamo di dati di closing
+        # we save closing data
         self.dataclose = self.datas[0].close
-        
-    #Il metodo next racchiude la strategia; 
-    #se siamo in posizione ci fa chiudere e ribaltare altrimenti 
-    #se non siamo in posizione ci fa entrare a mercato    
-             
+
+	#Il metodo next racchiude la strategia;
+    # The next method encompasses the strategy
+    #se siamo in posizione ci fa chiudere e ribaltare altrimenti
+    # if we are in position it makes us close and overturn otherwise
+    #se non siamo in posizione ci fa entrare a mercato
+    # if we are not in position it allows us to enter the market
+
     def next(self):
-        if self.position.size:
-            if self.buysig < 0:
-                self.close()
-                self.sell()
-            elif self.buysig > 0:
-                self.close()
-                self.buy()
-                    
-        else:
-            if self.buysig > 0:
-                self.buy()
-            elif self.buysig < 0:
-                self.sell()
-                
-    def stampa(self, txt, dt=None):
-        
-        #Funzione di stampe per capire cosa sta accadendo
+		if self.position.size:
+			if self.buysig < 0:
+				self.close()
+				self.sell()
+			elif self.buysig > 0:
+				self.close()
+				self.buy()
+
+		else:
+			if self.buysig > 0:
+				self.buy()
+			elif self.buysig < 0:
+				self.sell()
+
+	def stampa(self, txt, dt=None):
+		"""Print function to understand what is happening"""
+		#Funzione di stampe per capire cosa sta accadendo
         dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
-        
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            # ordine acquisto/vendita accettato
+		print('%s, %s' % (dt.isoformat(), txt))
+
+	def notify_order(self, order):
+		"""buy / sell order accepted"""
+		if order.status in [order.Submitted, order.Accepted]:
+			# ordine acquisto/vendita accettato
             return
 
-        # Verifica se ordine completato
+		# Verifica se ordine completato
+        # Check if order completed
         if order.status in [order.Completed]:
             if order.isbuy():
           #Stampiamo dettaglio di quantità, prezzo e commissioni
+          # We print quantity, price and commission details
                 self.stampa(
                     'ACQ ESEGUITO, QTY: %.2f, PREZZO: %.2f, COSTO: %.2f, COMM %.2f' %
                     (order.executed.size,
                      order.executed.price,
                      order.executed.value,
-                     order.executed.comm        
+                     order.executed.comm
                      ))
 
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
-            else:  # Vendita
+            else:  # Vendita or # Sale or # Sell ?
                 self.stampa('VEND ESEGUITA, QTY: %.2f, PREZZO: %.2f, COSTO: %.2f, COMM %.2f' %
                          (order.executed.size,
                           order.executed.price,
@@ -119,10 +139,10 @@ class Cross_Medie(bt.Strategy):
             self.stampa('Ordine Cancellato')
 
         self.order = None
-        
+
     # Questa funzione ci restituisce il P&L dell'operazione chiusa
-    
     def notify_trade(self, trade):
+        """This function gives us the P&L of the closed operation"""
         if not trade.isclosed:
             return
         self.stampa('PROFITTO OPERAZIONE, LORDO %.2f, NETTO %.2f' %
